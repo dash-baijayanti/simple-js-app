1f183de15d569ad96cfd8e231ced9f52bd61de0a
Baijayanti Dash <dash.baijayanti@gmail.com>
2024-04-05 11:33:30 +0200

    build: array
    input of arraylist named by pokemonList, which contain 3objects and 3keys.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-alert('Hallo World');
-let FavFood="Indian-Dishes";
-document.write(FavFood);
+let pokemonList = [
+  { pokName: "Bulbasaur", height: 7, types: ["grass", "poison"] },
+  { pokName: "Butterfree", height: 1.1, types: ["psychic", "normal"] },
+  { pokName: "Beedrill", height: 1, types: ["rock", "fire"] },
+];
+
+document.querySelector("button").addEventListener("click", () => {
+  let currentUserName = document.querySelector("#username").value;
+  document.querySelector("#username-key").innerText = currentUserName;
+  
+});
