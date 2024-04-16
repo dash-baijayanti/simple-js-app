fe8b0be3ff95d0c36e4d9bcad86a0b8080c12a94
Baijayanti Dash <dash.baijayanti@gmail.com>
2024-04-16 13:31:01 +0200

    docs: shows list
    using IIFE , execute only pokemon lists

--- script.js
+++ script.js
@@ -1,62 +1,93 @@
-let pokemonList = [
-  { pokName: "Bulbasaur", height: 7, types: ["grass", "poison"] },
-  { pokName: "Butterfree", height: 1.1, types: ["psychic", "normal"] },
-  { pokName: "Beedrill", height: 1, types: ["rock", "fire"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      pokName: "Bulbasaur",
+      height: 7,
+      types: ["grass", "poison"],
+    },
+    { 
+      pokName: "Butterfree", 
+      height: 1.1, 
+      types: ["psychic", "normal"] },
+    { 
+      pokName: "Beedrill", 
+      height: 1, 
+      types: ["rock", "fire"] },
+  ];
+  function getAll(){
+    return pokemonList;
+  }
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
 
-document.querySelector("button").addEventListener("click", () => {
-  let currentUserName = document.querySelector("#username").value;
-  document.querySelector("#username-key").innerText = currentUserName;
+  return {
+    getAll :getAll,
+    add:add
+  };
+})();
+ 
+// document.write(pokemonRepository.getAll());
+console.log(pokemonRepository.getAll());
 
-  // condition for submit button
-    if (currentUserName == "Bulbasaur") {
-      // document.querySelector("#pokheight").innerText = 0;
-      document.querySelector("#pokheight").innerText = pokemonList[0].height;
-      document.querySelector("#poktype").innerText = pokemonList[0].types;
-
-    } else if (currentUserName == "Butterfree") {
-      // document.querySelector("#pokheight").innerText = 0;
-      document.querySelector("#pokheight").innerText = pokemonList[1].height;
-      document.querySelector("#poktype").innerText = pokemonList[1].types;
-    } else if (currentUserName == "Beedrill") {
-      // document.querySelector("#pokheight").innerText = 0;
-      document.querySelector("#pokheight").innerText = pokemonList[2].height;
-      document.querySelector("#poktype").innerText = pokemonList[2].types;
-    } else {
-      document.write("wrong input");
-    }
-    // condition for image button
-    const showImageButton = document.getElementById("show-image");
-      const myImage = document.getElementById("image");
-      const myImage2 = document.getElementById("image-2");
-      const myImage3 = document.getElementById("image-3");
-      showImageButton.addEventListener("click", () => {
-        if(currentUserName=="Bulbasaur"){
-         myImage.hidden = !myImage.hidden;
-        }else if (currentUserName == "Butterfree") {
-          myImage2.hidden = !myImage2.hidden;
-        }else if (currentUserName == "Beedrill") {
-          myImage3.hidden = !myImage3.hidden;
-        
-        }else{
-          document.write('wrong input');
-        }
-      });
-   
+// using forEach() for showing the pokemonList details
+pokemonRepository.getAll().forEach(function (details) {
+  document.write(
+    "<p>=> Pokemon name:"+" "+details.pokName,
+    "<p> Height:" + "" + details.height,
+    "<p> Pok Types:" + " " + details.types
+    );
 });
+
+
+// document.querySelector("button").addEventListener("click", () => {
+//   let currentUserName = document.querySelector("#username").value;
+//   document.querySelector("#username-key").innerText = currentUserName;
+ 
+  // condition for submit button
+//   if (currentUserName == "Bulbasaur") {
+//     // document.querySelector("#pokheight").innerText = 0;
+//     document.querySelector("#pokheight").innerText = object.keys(pokemonRepository).height;
+//     document.querySelector("#poktype").innerText = pokemonList[0].types;
+//   } else if (currentUserName == "Butterfree") {
+//     // document.querySelector("#pokheight").innerText = 0;
+//     document.querySelector("#pokheight").innerText = pokemonList[1].height;
+//     document.querySelector("#poktype").innerText = pokemonList[1].types;
+//   } else if (currentUserName == "Beedrill") {
+//     // document.querySelector("#pokheight").innerText = 0;
+//     document.querySelector("#pokheight").innerText = pokemonList[2].height;
+//     document.querySelector("#poktype").innerText = pokemonList[2].types;
+//   } else {
+//     document.write("wrong input");
+//   }
+// })
+  // condition for image button
+//   const showImageButton = document.getElementById("show-image");
+//   const myImage = document.getElementById("image");
+//   const myImage2 = document.getElementById("image-2");
+//   const myImage3 = document.getElementById("image-3");
+//   showImageButton.addEventListener("click", () => {
+//     if (currentUserName == "Bulbasaur") {
+//       myImage.hidden = !myImage.hidden;
+//     } else if (currentUserName == "Butterfree") {
+//       myImage2.hidden = !myImage2.hidden;
+//     } else if (currentUserName == "Beedrill") {
+//       myImage3.hidden = !myImage3.hidden;
+//     } else {
+//       document.write("wrong input");
+//     }
+//   });
+// });
 // using for loop for showing the pokemonList details
-// for (let i = 0; i<pokemonList.length; i++){ 
+// for (let i = 0; i<pokemonList.length; i++){
 
 //   if(pokemonList[i].height<10 && pokemonList[i].height>5 ){
 //   document.write(`<p class="for">Pokname-${pokemonList[i].pokName},  (height-${pokemonList[i].height})-"Wow that's big",  types-[ ${pokemonList[i].types}]</p><br>`);
 //   }else if(pokemonList[i].height>=1){
-//     document.write(`<p class="for"> Pokname-${pokemonList[i].pokName},  (height-${pokemonList[i].height})-"Medium",  types-[ ${pokemonList[i].types}]</p><br>`); 
+//     document.write(`<p class="for"> Pokname-${pokemonList[i].pokName},  (height-${pokemonList[i].height})-"Medium",  types-[ ${pokemonList[i].types}]</p><br>`);
 //   }else{
 //     document.write(`<p class="for"> Pokname-${pokemonList[i].pokName}  (height-${pokemonList[i].height})-"Small",  types-[ ${pokemonList[i].types}]</p>`);
 //   }
 // };
 
-// using forEach() for showing the pokemonList details
-pokemonList.forEach (function(details){
-  document.write("<p>Pokemon name:</p>"+''+ details.pokName ,"<p>Height:</p>" +''+ details.height, "<p>Pok Types:</p>" +' '+ details.types);
-});
+
